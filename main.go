11fbc6cdd1204486739b5d45a2948304f93b19bf
Daniel <daniel_sousa.p@outlook.com>
2022-09-20 23:48:36 -0300

    feat: first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	r.Run(":3333")
+}
