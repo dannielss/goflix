11fd04acae906069be48bfa5429a958c7cc8a58f
Daniel <daniel_sousa.p@outlook.com>
2022-10-03 23:34:22 -0300

    feat: create loginController

---
+++ controller/loginController.go
@@ -0,0 +1,73 @@
+package controller
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/dannielss/goflix/config"
+	"github.com/dannielss/goflix/model"
+	"github.com/dannielss/goflix/repository"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func NewLoginController(userRepository repository.UserRepositoryInterface) LoginControllerInterface {
+	return &loginController{userRepository}
+}
+
+type loginController struct {
+	userRepository repository.UserRepositoryInterface
+}
+
+type LoginControllerInterface interface {
+	Login(c *gin.Context)
+}
+
+func (lc *loginController) Login(c *gin.Context) {
+	var payload model.Login
+
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var user model.User
+	row := lc.userRepository.ShowOne("email = ?", payload.Email)
+
+	scanError := row.Scan(&user.Id, &user.Name, &user.Email, &user.Password)
+
+	if scanError == sql.ErrNoRows {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Cannot find user",
+		})
+		return
+	}
+
+	if scanError != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Something wrong",
+		})
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password))
+
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Invalid credentials",
+		})
+		return
+	}
+
+	token, err := config.NewJWTConfig().GenerateToken(user.Id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": token,
+	})
+}
